7ebe8450930a9c6a269c93667f7204e050b67212
EduardoNogueira <edu020nogueira@gmail.com>
2022-09-25 11:58:37 -0300

    Atualização no create(página do link criado)

--- seurl/public/javascripts/copy.js
+++ seurl/public/javascripts/copy.js
@@ -3,7 +3,7 @@ const copyButton = document.getElementById("copy");
 
 copyButton.addEventListener('click', () =>{
     navigator.clipboard.writeText(textInput)
-    
+    copyButton.innerHTML = 'Link Copiado!'
 })
     
 
