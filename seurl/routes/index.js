e6b1b040ede1844629ecfefe8aa9801c66aef97a
EduardoNogueira <edu020nogueira@gmail.com>
2022-09-24 23:32:37 -0300

    atualização

--- seurl/routes/index.js
+++ seurl/routes/index.js
@@ -2,33 +2,8 @@ var express = require('express');
 var router = express.Router();
 const Link = require('../models/link');
 
-
-router.get('/:code/stats', async (req, res, next) =>{
-  const code = req.params.code;
-  const resultado = await Link.findOne({where: {code} });
-  if(!resultado) return res.sendStatus(404);
-  res.render('stats', resultado.dataValues);
-})
-
-router.get('/:code', async (req, res, next) =>{
-  const code = req.params.code;
-
-  const resultado = await Link.findOne({where: {code} });
-  if(!resultado) return res.sendStatus(404);
-
-  resultado.hits++;
-  await resultado.save();
-
-  /* Redenriza a página com anúncios e com o link */
-  res.render('link', resultado.dataValues)
-})
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
-
 /* gerador de codigos */
+
 function generateCode(){
   let text = '';
   const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
@@ -37,8 +12,10 @@ function generateCode(){
   return text;
 }
 
-
-
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
 
 /* criar nova url */
 router.post('/new', async (req, res, next) =>{
@@ -55,4 +32,26 @@ router.post('/new', async (req, res, next) =>{
     
 })
 
+router.get('/:code', async (req, res, next) =>{
+  const code = req.params.code;
+  
+  const resultado = await Link.findOne({where: {code} });
+  if(!resultado) return res.sendStatus(404);
+  
+  resultado.hits++; 
+  await resultado.save();
+  
+  /* Redenriza a página com anúncios e com o link */
+  res.render('link', resultado.dataValues)
+
+})
+
+router.get('/:code/stats', async (req, res, next) =>{
+  const code = req.params.code;
+  const resultado = await Link.findOne({where: {code} });
+  if(!resultado) return res.sendStatus(404);
+  res.render('stats', resultado.dataValues);
+})
 module.exports = router;
+
+
