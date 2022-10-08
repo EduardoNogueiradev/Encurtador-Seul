33ae443c53d5b0f61d7e94d3e065e8a91635ded5
EduardoNogueira <edu020nogueira@gmail.com>
2022-10-08 13:02:20 -0300

    alteração 3

--- seurl/public/javascripts/copy.js
+++ seurl/public/javascripts/copy.js
@@ -2,10 +2,12 @@ const textInput = document.getElementById("text");
 const copyButton = document.getElementById("copy");
 
 copyButton.addEventListener('click', () =>{
-    textInput.value.Document.execCommand('copy');
-    copyButton.innerHTML = 'Link Copiado!'
+  textInput.select();
+  textInput.setSelectionRange(0, 99999)
+  document.execCommand('copy');
+  copyButton.innerHTML = 'Link Copiado!'
 })
-    
+
 
 
 
